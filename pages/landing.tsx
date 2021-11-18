b69d33b0c048bf7dc0cbb882a00f50a5233f2a5c
kdo <dominik.kaegi@ti8m.ch>
2021-11-18 16:56:36 +0100

    Add Chakra Ui and Landing Page

---
+++ pages/landing.tsx
@@ -0,0 +1,14 @@
+import { CallToAction } from "../components/landing/CallToAction"
+import { Navigation } from "../components/landing/Navigation"
+
+
+const LandingPage: React.FC = () => {
+    return (
+        <>
+            <Navigation />
+            <CallToAction />
+        </>
+    )
+}
+
+export default LandingPage
